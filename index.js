f689c71441ead950b52a1794819d0bf0b5f06c63
Stefan Penner <stefan.penner@gmail.com>
2015-05-26 11:26:54 -0700

    time <path> rather then <path> time

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var fs = require('fs');
 require.extensions['.js'] = function(module, filename) {
-  console.time(filename);
+  var start = new Date();
   var content = fs.readFileSync(filename, 'utf8').toString();
   var mod = module._compile(content, filename);
-
-  console.timeEnd(filename);
+  console.log(new Date() - start, filename)
   return mod;
 };
 
